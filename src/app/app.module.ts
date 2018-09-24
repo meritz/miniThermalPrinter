22cb29698946e847d767efdf274d0c5f76584912
modestemeritz@gmail.com <modestemeritz@gmail.com>
2018-09-24 08:53:22 +0000

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { ErrorHandler, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HomePage } from '../pages/home/home';
+import { PrinterProvider } from './../providers/printer/printer';
+import { MyApp } from './app.component';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BluetoothSerial,
+    PrinterProvider
+  ]
+})
+export class AppModule {}
